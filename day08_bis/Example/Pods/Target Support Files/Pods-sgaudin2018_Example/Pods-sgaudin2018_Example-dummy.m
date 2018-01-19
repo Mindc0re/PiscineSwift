3e346101ba9c455aee872bc1ff5ae2db8ca487b4
Simon GAUDIN <sgaudin@e2r12p20.42.fr>
2018-01-19 13:58:50 +0100

    yey

--- day08_bis/Example/Pods/Target Support Files/Pods-sgaudin2018_Example/Pods-sgaudin2018_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sgaudin2018_Example : NSObject
-@end
-@implementation PodsDummy_Pods_sgaudin2018_Example
-@end
--- day08_bis/Example/Pods/Target Support Files/Pods-sgaudin2018_Tests/Pods-sgaudin2018_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sgaudin2018_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_sgaudin2018_Tests
-@end
